c6ab06e484a9041138fa4e8d1f17c36d0b1ef5c3
Łukasz Adamczewski <tworzenieweb@gmail.com>
2012-09-24 15:58:08 +0200

    added user bundle

---
+++ src/Application/Sonata/UserBundle/Controller/UserController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Application\Sonata\UserBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Application\Sonata\UserBundle\Entity\User;
+
+/**
+ * User controller.
+ *
+ * @Route("/user")
+ */
+class UserController extends Controller
+{
+    /**
+     * Lists all User entities.
+     *
+     * @Route("/", name="user")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('ApplicationSonataUserBundle:User')->findAll();
+
+        return array(
+            'entities' => $entities,
+        );
+    }
+
+    /**
+     * Finds and displays a User entity.
+     *
+     * @Route("/{id}/show", name="user_show")
+     * @Template()
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('ApplicationSonataUserBundle:User')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find User entity.');
+        }
+
+        return array(
+            'entity'      => $entity,
+        );
+    }
+
+}
